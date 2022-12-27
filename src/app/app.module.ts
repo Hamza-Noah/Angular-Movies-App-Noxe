10e2ac947b80c1b367a66c64ac1a07a46c282ccd
Hamza-Noah <hamza.nouh97@gmail.com>
2022-12-27 16:58:38 +0200

    starting structuring the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutComponent } from './about/about.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HomeComponent } from './home/home.component';
+import { TvComponent } from './tv/tv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    NavbarComponent,
+    GalleryComponent,
+    RegisterComponent,
+    LoginComponent,
+    NotFoundComponent,
+    AboutComponent,
+    MovieDetailsComponent,
+    ContactsComponent,
+    HomeComponent,
+    TvComponent
   ],
   imports: [
     BrowserModule,
