c8e779b475a9550d45d7ea6be0abfb309d4e68d5
Hamza-Noah <hamza.nouh97@gmail.com>
2022-12-28 09:02:43 +0200

    added the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { TvComponent } from './tv/tv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path: '', redirectTo: "HomeComponent"},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'movie', component: AboutComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: '**', component: NotFoundComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
